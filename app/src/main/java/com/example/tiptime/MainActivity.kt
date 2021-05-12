39822351cd1ed976342a49682efd523d4891bc63
pandakartik663 <46396204+pandakartik663@users.noreply.github.com>
2021-05-12 17:29:19 +0530

    Completed the Main Activity Function to Generate the exact amount of tip!

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.tiptime
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,14 +15,28 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //binding.btnCalculate.setOnClickListener { calculateTip() }
+        binding.btnCalculate.setOnClickListener { calculateTip() }
 
     }
-    /*fun calculateTip(){
+
+    fun calculateTip() {
         val stringInTextField = binding.etCostOfService.text.toString()
         val cost = stringInTextField.toDouble()
-    }
+        val selectedId = binding.rgTip.checkedRadioButtonId
+        val tipPrecentage = when (selectedId) {
+            R.id.rbTwentyPercent -> 0.20
+            R.id.rbFifteenPercent -> 0.15
+            else -> 0.10
+        }
+        var tip = cost * tipPrecentage
+        val roundUp = binding.swcRoundUp.isChecked
+        if (roundUp) {
+            tip = ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
 
-     */
+        binding.tvResult.text = getString(R.string.Tip_Amount, formattedTip)
+
+    }
 
 }
